4a52ddd0ac01d0c315bbca6c4f3bf4e46b7451bd
Aleksandr <bixber.official@gmail.com>
2023-11-21 13:35:47 +0300

    Set up layouts for the MainPage (catalog)

--- SportResort/Pages/MainPage.xaml.cs
+++ SportResort/Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,26 @@ using System.Windows.Shapes;
 
 namespace SportResort.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для MainPage.xaml
-    /// </summary>
     public partial class MainPage : Page
     {
+        public ObservableCollection<Product> Products { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            DataContext = this;
+
+            Products = new ObservableCollection<Product>();
+            Products.Add(new Product { ImageUrl = "/Media/Products/Images/ProfileIcon.png", Title = "Продукт 1", Price = 10 });
+            Products.Add(new Product { ImageUrl = "/Media/Products/Images/Product2.png", Title = "Продукт 2", Price = 20 });
+            Products.Add(new Product { ImageUrl = "/Media/Products/Images/Product3.png", Title = "Продукт 3", Price = 30 });
         }
+    }
+
+    public class Product
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
     }
 }
