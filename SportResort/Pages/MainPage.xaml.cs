28c7a18a4fd8ea893097b128805a013e3775955a
Aleksandr <bixber.official@gmail.com>
2023-11-21 21:11:15 +0300

    Full create XAML-layout for the page of products catalog

--- SportResort/Pages/MainPage.xaml.cs
+++ SportResort/Pages/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,24 +19,16 @@ namespace SportResort.Pages
 {
     public partial class MainPage : Page
     {
-        public ObservableCollection<Product> Products { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
-            DataContext = this;
-
-            Products = new ObservableCollection<Product>();
-            Products.Add(new Product { ImageUrl = "/Media/Products/Images/ProfileIcon.png", Title = "Продукт 1", Price = 10 });
-            Products.Add(new Product { ImageUrl = "/Media/Products/Images/Product2.png", Title = "Продукт 2", Price = 20 });
-            Products.Add(new Product { ImageUrl = "/Media/Products/Images/Product3.png", Title = "Продукт 3", Price = 30 });
+            using (var dbContext = new SportStoreEntities())
+            {
+                IControlProductsList.ItemsSource = dbContext.Products.ToList();
+            }
         }
     }
 
-    public class Product
-    {
-        public string ImageUrl { get; set; }
-        public string Title { get; set; }
-        public decimal Price { get; set; }
-    }
+    
 }
---
+++ SportResort/Utils/Converters/IsAvailableTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SportResort.Utils.Converters
+{
+    public class IsAvailableTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isAvailable = (bool)value;
+
+            // Конвертация в текст
+            string text = isAvailable ? "В наличии" : "Нет в наличии";
+
+            // Конвертация в цвет
+            Brush color = isAvailable ? Brushes.Green : Brushes.Red;
+
+            // Возвращаем результат в зависимости от параметра
+            if (parameter != null && parameter.ToString() == "Foreground")
+                return color;
+            else
+                return text;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
