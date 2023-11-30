3c3a4525eaa05f77f1ac29a0f81b57fcd6f0dc3f
Aleksandr Bynkov <bixber.commercial@yandex.ru>
2023-11-30 20:51:32 +0300

    Clean small unnecessary image data

---
+++ SportResort/Pages/ProductDetailPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SportResort.Commands;
+
+
+namespace SportResort.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductDetailPage.xaml
+    /// </summary>
+    public partial class ProductDetailPage : Page
+    {
+        public Products SelectedProduct { get; set; }
+        public short userRoleId { get; set; }
+
+        public ProductDetailPage(Products selectedProduct, short userRoleId)
+        {
+            InitializeComponent();
+            SetPermission();
+            SelectedProduct = selectedProduct;
+            DataContext = new ButtonCommandsViewModel()
+            {
+                SelectedProduct = SelectedProduct
+            };
+             
+        }
+
+        private void buttonEditProduct_onClick(object sender, RoutedEventArgs e)
+        {
+            Products editable_product = SelectedProduct;
+            bool saveButtonVisible = true;
+            NavigationService?.Navigate(new ProductFormPage(editable_product, saveButtonVisible));
+        }
+
+        private void buttonDeleteProduct_onClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите удалить этот товар?", "Подтверждение действия", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    using (var dbContext = new SportResortEntities())
+                    {
+                        dbContext.Products.Attach(SelectedProduct);
+                        dbContext.Products.Remove(SelectedProduct);
+                        dbContext.SaveChanges();
+                    }
+                    NavigationService?.Navigate(new MainPage(userRoleId));
+                    MessageBox.Show($"Товар {SelectedProduct.title} был успешно удален из каталога", "Удаление товара", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при удалении товара: " + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void buttonAddToCart_onClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetPermission()
+        {
+            if (userRoleId == 1)
+            {
+                editProductStackPanel.Visibility = Visibility.Visible;
+            }
+            else if (userRoleId == 2)
+            {
+                editProductStackPanel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                editProductStackPanel.Visibility = Visibility.Hidden;
+            }
+        }
+
+    }
+}
---
+++ SportResort/Pages/ProductFormPage.xaml.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Data.Entity.Migrations;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace SportResort.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProductPage.xaml
+    /// </summary>
+    public partial class ProductFormPage : Page
+    {
+        public bool SaveButtonVisible { get; set; }
+        private byte[] imageBytes;
+        private string imageFilePath;
+        public short userRoleId { get; set; }
+        public Products EditableProduct { get; set; }
+        
+        public ProductFormPage(Products editableProduct, bool saveButtonVisible)
+        {
+            InitializeComponent();
+            EditableProduct = editableProduct;
+            DataContext = EditableProduct;
+            this.userRoleId = userRoleId;
+
+            if (saveButtonVisible)
+            {
+                buttonSaveChangesOfProduct.Visibility = Visibility.Visible;
+                buttonAddProduct.Visibility = Visibility.Hidden;
+                buttonAddAnotherYetProduct.Visibility = Visibility.Hidden;
+            }
+        }
+        private void costTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonBrowseImage_onClick(object sender, RoutedEventArgs e)
+        {
+            string imagePath = SelectImage();
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(imagePath));
+                imageBytes = File.ReadAllBytes(imagePath);
+                imageFilePath = imagePath;
+            }
+        }
+
+        private string SelectImage()
+        {
+            string imagePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imagePath = openFileDialog.FileName;
+            }
+
+            return imagePath;
+        }
+
+        private void SaveImageToWorkingDirectory()
+        {
+            if (!string.IsNullOrEmpty(imageFilePath))
+            {
+                string projectDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName;
+                string imagesFolderPath = Path.Combine(projectDirectory, "Media", "Products", "Images");
+                Directory.CreateDirectory(imagesFolderPath);
+                string fileName = $"{Guid.NewGuid()}{Path.GetExtension(imageFilePath)}";
+                string destinationPath = Path.Combine(imagesFolderPath, fileName);
+
+                File.Copy(imageFilePath, destinationPath);
+            }
+        }
+            
+
+        private Dictionary<string, object> ParseFieldsAndCompleteObject()
+            {
+                bool answer = false;
+
+                if (yesRadioButton.IsChecked == true)
+                {
+                    answer = true;
+                }
+                else if (noRadioButton.IsChecked == true)
+                {
+                    answer = false;
+                }
+
+                decimal cost;
+                bool isValidCost = decimal.TryParse(productCostTextBox.Text, out cost);
+
+                if (!isValidCost)
+                {
+                    MessageBox.Show("Указано некорректное значение стоимости товара.");
+                    return null;
+                }
+
+                Dictionary<string, object> productAttributes = new Dictionary<string, object>
+                {
+                    { "title", productTitleTextBox.Text },
+                    { "description", productDescriptionTextBox.Text },
+                    { "cost", cost },
+                    { "is_available", answer },
+                    { "image", imageBytes }
+                };
+
+                return productAttributes;
+            }
+
+        private void buttonAddAnotherYetProduct_onClick(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, object> productAttributes = ParseFieldsAndCompleteObject();
+
+            if (productAttributes != null)
+            {
+                Products product = new Products
+                {
+                    title = (string)productAttributes["title"],
+                    description = (string)productAttributes["description"],
+                    cost = (decimal)productAttributes["cost"],
+                    is_available = (bool)productAttributes["is_available"],
+                    image = (byte[])productAttributes["image"]
+                };
+                SaveProductToDatabase(product);
+            }
+            SaveImageToWorkingDirectory();
+            ClearFormFields();
+        }
+
+        private void buttonAddProduct_onClick(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, object> productAttributes = ParseFieldsAndCompleteObject();
+
+            if (productAttributes != null)
+            {
+                Products product = new Products
+                {
+                    title = (string)productAttributes["title"],
+                    description = (string)productAttributes["description"],
+                    cost = (decimal)productAttributes["cost"],
+                    is_available = (bool)productAttributes["is_available"],
+                    image = (byte[])productAttributes["image"]
+                };
+                SaveProductToDatabase(product);
+            }
+
+            SaveImageToWorkingDirectory();
+            NavigationService?.Navigate(new MainPage(userRoleId));
+        }
+
+        private void SaveProductToDatabase(Products product)
+        {
+            try
+            {
+                using (var dbContext = new SportResortEntities())
+                {
+                    dbContext.Products.Add(product);
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при добавлении товара в базу каталоге: {ex.Message}" +
+                    $"\n\nСкорее всего Вы не ввели данные в обязательные поля или ввели их неверно.", "Ошибка изменения товара");
+            }
+        }
+
+        private void EditProductToDatabase(Products product)
+        {
+            try
+            {
+                using (var dbContext = new SportResortEntities())
+                {
+                    var existingProduct = dbContext.Products.Find(product.id);
+
+                    if (existingProduct != null)
+                    {
+                        dbContext.Entry(existingProduct).CurrentValues.SetValues(product);
+                        dbContext.SaveChanges();
+                        MessageBox.Show("Товар был успешно изменен в каталоге!", "Успешное изменение");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Товар не найден.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при изменении товара в каталоге: {ex.Message}" +
+                    $"\n\nСкорее всего Вы не ввели данные в обязательные поля или ввели их неверно.", "Ошибка изменения товара");
+            }
+        }
+
+        private void ClearFormFields()
+        {
+            productTitleTextBox.Text = string.Empty;
+            productDescriptionTextBox.Text = string.Empty;
+            productCostTextBox.Text = string.Empty;
+            noRadioButton.IsChecked = false;
+            yesRadioButton.IsChecked = false;
+            string imagePath = "\\Media\\Icon\\EmptyImage.png";
+            BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+            imgPhoto.Source = bitmapImage;
+        }
+
+        private void buttonSaveChangesOfProduct_onClick(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, object> productAttributes = ParseFieldsAndCompleteObject();
+
+            if (productAttributes != null)
+            {
+                Products product = new Products
+                {
+                    
+                    id = EditableProduct.id, // Устанавливаем ID изменяемого товара
+                    title = (string)productAttributes["title"],
+                    description = (string)productAttributes["description"],
+                    cost = (decimal)productAttributes["cost"],
+                    is_available = (bool)productAttributes["is_available"],
+                    image = (byte[])productAttributes["image"]
+                };
+                EditProductToDatabase(product);
+            }
+
+            SaveImageToWorkingDirectory();
+            
+        } 
+    }
+}
